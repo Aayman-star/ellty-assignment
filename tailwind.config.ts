aff34653eeddc09c973e920a3fcd5dbfbc2b57a5
Aayman-star <aayman.khalid1@gmail.com>
2024-08-15 22:05:32 +0500

    switched the values for drop-shadow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       boxShadow: {
-        "shadow-default": "0 0 4px rgba(20, 20, 20, 0.1) ",
-        "shadow-hover": "0 8px 15px rgba(20, 20, 20, 0.12)",
+        "shadow-default": "0 8px 15px rgba(20, 20, 20, 0.12)",
+        "shadow-hover": "0 0 4px rgba(20, 20, 20, 0.1) ",
+        //
       },
     },
   },
